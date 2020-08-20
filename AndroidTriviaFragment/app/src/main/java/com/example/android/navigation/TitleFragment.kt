5666c4a0cf86af528f4b13117d8b13038d529c7f
Edward Santiesteban <Edward.Santiesteban@aa.com>
2020-08-20 10:19:56 -0500

    Added options menu

--- AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -14,35 +14,40 @@
  * limitations under the License.
  */
 
- package com.example.android.navigation
+package com.example.android.navigation
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
-                R.layout.fragment_title,container,false)
+                R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController()) ||
+                super.onOptionsItemSelected(item)
+    }
 }
